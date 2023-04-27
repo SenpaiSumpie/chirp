52025d0a041bfc3dcde32545f3559c8cc5a34539
SenpaiSumpie <max.lee.mosier@gmail.com>
2023-04-27 16:07:48 -0500

    seperate ssg helper, full post page

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,26 +1,52 @@
 
-import { type NextPage } from "next";
+import type { GetStaticProps,  NextPage } from "next";
 import Head from "next/head";
+import { api } from "~/utils/api";
+import { PageLayout } from "~/components/layout";
+import { PostView } from "~/components/postView";
+import { generateSSGHelper } from "~/server/helpers/ssgHelper";
 
 
-
-const SinglePostPage: NextPage = () => {
+const SinglePostPage: NextPage<{id: string}> = ({id}) => {
   
+  const { data} = api.posts.getById.useQuery({
+    id,
+  });
+
+  if( !data ) return <div>404</div>;
 
   return (
     <>
       <Head>
-        <title>Post</title>
+        <title>{`${data.post.content} - @${data.author.username}`}</title>
       </Head>
-      <main className="flex h-screen justify-center ">
-        <div className="w-full h-full md:max-w-2xl border-x border-slate-400">
-          <div className="flex border-b border-slate-400 p-4">
-            Post View
-          </div>
-        </div>
-      </main>
+      <PageLayout>
+        <PostView {...data}/>
+      </PageLayout>
     </>
   );
+};
+
+
+export const getStaticProps : GetStaticProps = async (context) => {
+  const ssg = generateSSGHelper();
+
+  const id = context.params?.id;
+
+  if( typeof id !== "string") throw new Error("no id");
+
+  await ssg.posts.getById.prefetch({ id });
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      id
+    },
+  }
+};
+
+export const getStaticPaths = () => {
+  return {paths: [], fallback: "blocking"}
 };
 
 export default SinglePostPage;
