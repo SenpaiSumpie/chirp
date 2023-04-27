f181a31e9011725451488c580c190d6997a957c3
SenpaiSumpie <max.lee.mosier@gmail.com>
2023-04-27 13:39:30 -0500

    Core functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,11 +10,22 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
 import { LoadingPage } from "~/components/loading";
+import { useState } from "react";
 
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const { user } = useUser();
+  const [ input, setInput ] = useState("");
+  
+  const ctx = api.useContext();
+
+  const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
+    onSuccess: () => {
+      setInput("");
+      void ctx.posts.getAll.invalidate();
+    },
+  });
   
   if( !user ) return null;
 
@@ -27,7 +38,15 @@ const CreatePostWizard = () => {
         width={56}
         height={56}
       />
-      <input placeholder="Type some emojis!" className="bg-transparent grow outline-none"/>
+      <input 
+        placeholder="Type some emojis!" 
+        className="bg-transparent grow outline-none"
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        disabled={isPosting}
+      />
+      <button onClick={() => mutate({content: input})}>Post</button>
     </div>
   );
 };
@@ -69,7 +88,7 @@ const Feed = () => {
 
   return(
     <div className="flex flex-col">
-      {data?.map((fullPost) => (
+      {data.map((fullPost) => (
         <PostView {...fullPost} key={fullPost.post.id}/>
       ))}
     </div>
